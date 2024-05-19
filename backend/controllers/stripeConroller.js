3d9c8a6d1d794abe8906c82dd784412419e59731
Ujk768 <utkarsh.kanade1@gmail.com>
2024-05-19 17:14:02 +0530

    added stripe checkout

--- backend/controllers/stripeConroller.js
+++ backend/controllers/stripeConroller.js
@@ -1,4 +1,5 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const uuid = require("uuid");
 
 const paymentIntent = async (req, res) => {
   const { amount } = req.body;
@@ -12,7 +13,7 @@ const paymentIntent = async (req, res) => {
   }
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      currency: "INR",
+      currency: "inr",
       amount: amount,
       automatic_payment_methods: { enabled: true },
     });
@@ -36,7 +37,75 @@ const getConfig = async (req, res) => {
   });
 };
 
+const createCheckoutSession = async (req, res) => {
+  try {
+    const cartItems = req.body;
+    console.log(cartItems);
+    if (cartItems) {
+      try {
+        const lineItems = cartItems.map((item) => ({
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: item.title,
+              images: [item.thumbnail],
+            },
+            unit_amount: (Math.round(item.discountedPrice)) * 100,
+          },
+          quantity: 1,
+        }));
+        const session = await stripe.checkout.sessions.create({
+          line_items: lineItems ? lineItems : [],
+          mode: "payment",
+          shipping_address_collection: {
+            allowed_countries: ["IN"],
+          },
+          payment_method_types: ["card"],
+          success_url: "http://localhost:3000/success",
+          cancel_url: "http://localhost:3000/",
+        });
+        res.json({ id: session.id });
+      } catch (err) {
+        console.log(err);
+        res.status(400).send({
+          message: err.message,
+        });
+      }
+    } else {
+      res.status(400).send("cart details not passed in req body");
+    }
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+};
+
+const createCustomer = async (req, res) => {
+  const { custName, custId } = req.body;
+  try {
+    const customer = await stripe.customers.create({
+      name: custName,
+      //   id: custId,
+      address: {
+        // line1: address.line1,
+        // city: address.city,
+        // postal_code: address.postal_code,
+        // state: address.state,
+        country: "IN",
+      },
+    });
+    res.status(200).send({
+      message: "Customer created",
+    });
+  } catch (err) {
+    console.log("error", err);
+    res.status(400).send({ message: err.message });
+  }
+};
+
+
 module.exports = {
   paymentIntent,
   getConfig,
+  createCheckoutSession,
+  createCustomer,
 };
--- backend/routes/stripeRoutes.js
+++ backend/routes/stripeRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const {paymentIntent, getConfig} = require("../controllers/stripeConroller");
+const {
+  paymentIntent,
+  getConfig,
+  createCheckoutSession,
+  createCustomer,
+} = require("../controllers/stripeConroller");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/create-payment-intent", protect, paymentIntent);
 router.get("/config", protect, getConfig);
+router.post("/create-checkout-session", createCheckoutSession);
+router.post("/create-customer", createCustomer);
 
 module.exports = router;
